9254c8a7eafc219d6780c91073e08efc7dee5ce8
Siddarth_tak <siddarth.tak03@gmail.com>
2024-03-20 21:42:15 +0530

    d

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # Add 'overweight' column
-df['overweight'] = None
+height = df['height']/100
+bmi = df['weight']/(height**2)
+df['BMI'] = bmi
+list1 = []
+for i in range(70000):
+  if df['BMI'][i] > 25:#<---- very very import how to access individual elements in series or list or any thing mention[i]index here
+    list1.append(1)
+  else:
+    list1.append(0)
+df['overweight'] = list1
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+df['cholesterol'].replace({1: 0, 2: 1, 3: 1}, inplace=True)
+df['gluc'].replace({1: 0, 2: 1, 3: 1}, inplace=True)
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
-
-
+    df_cat = df.melt(id_vars = 'cardio', 
+                     value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], 
+                     value_name='value')
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
+    df_cat =  pd.DataFrame({'total':df_cat.groupby(['cardio', 'variable'])['value'].value_counts()})\
+                                     .rename(columns={'cardio':'Cardio','variable':'Variable', 'value':'Value'})\
+                                     .reset_index()
+
 
     # Draw the catplot with 'sns.catplot()'
+    catplot = sns.catplot(data=df_cat, x='variable', y='total', col='cardio', kind='bar', hue='value')
+
 
 
 
     # Get the figure for the output
-    fig = None
+    fig =  catplot.fig
 
 
     # Do not modify the next two lines
